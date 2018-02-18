5a9cae53f468ae14920eb1300ea85f5d39e6d317
kamc <steam-punker@mail.ru>
2018-02-18 16:33:54 +0300

    Create gallery.php

---
+++ lesson3/gallery.php
@@ -0,0 +1,19 @@
+<?php
+$img = include __DIR__ . '/data.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gallery</title>
+</head>
+<body>
+<?php
+foreach ($img as $key => $value) :?>
+    <a href="/image.php?id=<?php echo $key; ?>"><img src="/img/<?php echo $value; ?>" width="300"></a>
+<?php endforeach; ?>
+</body>
+</html>
