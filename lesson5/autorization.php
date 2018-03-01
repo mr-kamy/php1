3a0359acd904882a3b040020497e74a93274a884
kamc <steam-punker@mail.ru>
2018-03-01 22:10:53 +0300

    Create autorization.php

---
+++ lesson5/autorization.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include __DIR__ . '/functions.php';
+
+if ((isset($_POST['login'])) && (isset($_POST['password']))) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    if (existsUser($login)) {
+        if (checkPassword($login, $password)) {
+            $_SESSION['name'] = $login;
+            header('Location: /');
+        }
+    }
+}
+
+header('Location: /login.php');
