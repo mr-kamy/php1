be8225022287cd9f882ffdf67bcceea7b6cbc95d
kamc <steam-punker@mail.ru>
2018-03-11 20:41:40 +0300

    Update guestBook.php

--- lesson6/guestBook.php
+++ lesson6/guestBook.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/classes/GuestBook.php';
-require_once __DIR__ . '/functions.php';
 
-if (null != getCurrentUser()) {
-    $name = getCurrentUser();
 }
 
 $path = __DIR__ . '/data/guestbook.txt';
