cc8042d2d6661b5c6a87047649209345b53a9969
kamc <steam-punker@mail.ru>
2018-03-14 23:04:39 +0300

    Add files via upload

---
+++ lesson7/data/users.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'Иван' => '$2y$10$fweVvKRfpPgbcZJeUCWRiuf01L47wfSIMJ5xKWfnigK776zxoHIza',
+    'Василий' => '$2y$10$.mbwmQsKPaN4kg4j2C8DKuyHa2mB72H2kMFlsLeAJct9oxDUsiBGS',
+    'Петр' => '$2y$10$AxIlwqn68qxmqGImOfVkc.jXi7lvDRmJvDbIHB52kWWjyhG2v0Cgu',
+    'Мария' => '$2y$10$ZeJhOtY7CV2THLM/X9kjAeuEjzO6YbbGY7BGnBb2X0wDUGHpl7cju',
+];
