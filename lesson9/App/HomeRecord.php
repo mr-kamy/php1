4cd48d908104590b3221be2ea5101c83b955b73d
mr-kamy <steam-punker@mail.ru>
2018-03-29 15:13:53 +0300

    class HomeRecord

--- lesson9/App/HomeRecord.php
+++ lesson9/App/HomeRecord.php
@@ -5,12 +5,23 @@ namespace App;
 
 class HomeRecord
 {
+    protected $data;
+    protected $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+        $this->data = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    }
 
     public function getHomeRecord($path)
     {
-        $this->path = $path;
-        $record = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        return $record[0];
+        return $this->data;
+    }
+
+    public function updateHomeRecord($data)
+    {
+        file_put_contents($this->path, $data);
     }
 
 }
